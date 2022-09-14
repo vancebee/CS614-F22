30bead6024bf652df585cf96761cec8f68b3aa07
Tristan Tran <tristntran@gmail.com>
2022-09-14 12:30:38 -0700

    fixing a typo

--- src/preprocessdata.R
+++ src/preprocessdata.R
@@ -10,14 +10,13 @@ data_key = data_key %>% filter(ColumnLabel != "A2_6")
 # In class activity 1 Reverse code labeling
 # this is getting a list of columns. Can be replaced by another function
 # for instance names(raw_data[indices])
-reverse_code_lables <- data_key %>% filter(ReverseCode) %>%
+reverse_code_labels <- data_key %>% filter(ReverseCode) %>%
   select(ColumnLabel)
 
-
+# use mutate_at to reverse the code label at the specified columns
 # could alternately use r's built in [] filter and use lapply to column indices
-processed_data <- raw_data  %>% mutate_at(reverse_code_lables$ColumnLabel,
+processed_data <- raw_data  %>% mutate_at(reverse_code_labels$ColumnLabel,
                                     function(x) 6-x)
-processed_data %>% select(reverse_code_lables$ColumnLabel)
 
 # In class activity 2 change certain columns to numeric
 numeric_labels <- data_key %>% filter(Type == "Scalar") %>%
@@ -28,6 +27,7 @@ processed_data <- processed_data  %>% mutate_at(numeric_labels$ColumnLabel,
 
 # Àctivity 3
 # Collapse columns in to certain factors
+# http://derekogle.com/fishR/2018-03-30-Collapsing_Values
 
 # collapse demo info into three categories
 
