c125e4cd730c56519239feb6e09dcd92a2df7359
Tristan Tran <tristntran@gmail.com>
2022-09-20 15:49:16 -0700

    adding in collapsing categories

--- src/preprocessdata.R
+++ src/preprocessdata.R
@@ -29,6 +29,29 @@ processed_data <- processed_data  %>% mutate_at(numeric_labels$ColumnLabel,
 # Collapse columns in to certain factors
 # http://derekogle.com/fishR/2018-03-30-Collapsing_Values
 
-# collapse demo info into three categories
-
-# change continuous scale into factor scale
+# a28_2 3+4
+processed_data$A28_2 <- plyr::mapvalues(processed_data$A28_2,
+                                        from = c("4"),
+                                        to = c("3"))
+# a28_3 4+6+7+8
+# Drop 4
+processed_data$A28_3 <- plyr::mapvalues(processed_data$A28_3,
+                                        from = c("4","6","7","8"),
+                                        to = c("4"))
+# a28_6 drop 2
+processed_data$A28_6 <- plyr::mapvalues(processed_data$A28_6,
+                                        from = c("2"),
+                                        to = c(NA))
+# a28_7 3+4
+processed_data$A28_7 <- plyr::mapvalues(processed_data$A28_7,
+                                        from = c("4"),
+                                        to = c("3"))
+# a28_8 leave alone
+# a28_9 3+4+5
+processed_data$A28_9 <- plyr::mapvalues(processed_data$A28_9,
+                                        from = c("3","4","5"),
+                                        to = c("3"))
+# a28_10 3+4+5+8
+processed_data$A28_10 <- plyr::mapvalues(processed_data$A28_10,
+                                        from = c("3","4","5", "8"),
+                                        to = c("3"))
